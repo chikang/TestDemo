351ea9fc4690a689227eb5809bb58f6b9a544a80
池康 <18336072309@163.com>
2020-03-27 17:02:56 +0800

    1123123

---
+++ CCCCCCCSDK.framework/Headers/CCCCCCCSDK.h
@@ -0,0 +1,19 @@
+//
+//  CCCCCCCSDK.h
+//  CCCCCCCSDK
+//
+//  Created by 池康 on 2020/3/27.
+//  Copyright © 2020 CK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCCCCCCSDK : NSObject
+
+//网络请求
+- (void)postData;
+@end
+
+NS_ASSUME_NONNULL_END
